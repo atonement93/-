34f9a0c735c06b4d4510c1b12d0daba45e81d800
atonement93 <85914639+atonement93@users.noreply.github.com>
2021-12-19 15:05:21 +0900

    반응형 웹(2)

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const bars = document.querySelector(".fas");
+const minimenu = document.querySelector("#minimenu");
+
+function onClick() {
+    minimenu.classList.remove("hidden");
+}
+
+bars.addEventListener("click", onClick);
 
